96f4f69c10208402b26641d67222416c144e6c92
asantetg0154 <asantetg0154@gmail.com>
2026-08-26 16:54:49 +0000

    fix: route CDEJ ONE correctly under GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,29 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Route, Router as WouterRouter, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 
+const basePath = import.meta.env.BASE_URL.replace(/\/$/, "") || "/";
+
 function Router() {
-  return <Switch>
-    <Route path="/" component={Home} />
-    <Route path="/tableau-de-bord" component={Home} />
-    <Route path="/hub" component={Home} />
-    <Route path="/importer" component={Home} />
-    <Route path="/planification" component={Home} />
-    <Route path="/analyses" component={Home} />
-    <Route path="/workflows" component={Home} />
-    <Route path="/documents" component={Home} />
-    <Route path="/ask-cdej" component={Home} />
-    <Route path="/404" component={NotFound} />
-    <Route component={NotFound} />
-  </Switch>;
+  return <WouterRouter base={basePath}>
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/tableau-de-bord" component={Home} />
+      <Route path="/hub" component={Home} />
+      <Route path="/importer" component={Home} />
+      <Route path="/planification" component={Home} />
+      <Route path="/analyses" component={Home} />
+      <Route path="/workflows" component={Home} />
+      <Route path="/documents" component={Home} />
+      <Route path="/ask-cdej" component={Home} />
+      <Route path="/404" component={NotFound} />
+      <Route component={NotFound} />
+    </Switch>
+  </WouterRouter>;
 }
 
 export default function App() {
